f798404a182e5dde86104a28e6c05b49573975c9
zis76 <76632633+zis76@users.noreply.github.com>
2026-07-28 00:14:13 -0500

    Add route for Cleaderboard component

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,12 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import ListPacks from './pages/ListPacks.js';
+import Cleaderboard from './pages/cpleaderbroard.js';
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/listpacks', component: ListPacks },
+    { path: '/qualityboard', component: Cleaderboard },
 ];
